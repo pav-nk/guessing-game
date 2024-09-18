dc49db1d8851cef9e79389b61448820fff76012d
pav_nk <pav_nk@vk.com>
2024-09-18 11:30:09 +0300

    feat: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.guessNum = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNum = Math.ceil(this.min + (this.max - this.min) / 2);
+        return this.guessNum;
     }
 
     lower() {
-
+        this.setRange(this.min, this.guessNum);
     }
 
     greater() {
-
+        this.setRange(this.guessNum, this.max);
     }
 }
 
